3366c16cd182e0a7958e73d4c8792aba8bc3ccad
pisue <pisue22@naver.com>
2024-09-29 18:35:27 +0900

    Spring Initial

---
+++ src/main/java/com/hh99/hhplus_lecture/HhplusLectureApplication.java
@@ -0,0 +1,13 @@
+package com.hh99.hhplus_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hh99/hhplus_lecture/HhplusLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh99.hhplus_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
